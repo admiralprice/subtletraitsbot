240c2aee55fc6bf68e478a37e6b4f02bf71eb6cc
thephoenix01 <soumyadeep.das.phy14@iitbhu.ac.in>
2020-05-11 13:28:43 +0530

    - Added support for multiple subs. - Will search for new, rising, hot and top - Will check for archived and nsfw posts

--- indianthingsbot.py
+++ indianthingsbot.py
@@ -3,57 +3,153 @@ import pdb
 import re
 import os
 
-SUBREDDIT_NAME = 'bakchodi'
-KEYWORDS = ['just indian', 'indian things', 'subtle indian', 'indian traits', 'bakchodi']
-USERNAME = 'justindianstuff'
-PASSWORD = 'Robinishood69'
-CLIENT_ID = 'VRi20mq9xuDKuQ'
-CLIENT_SECRET = 'YRp4P0Ep6p0ewL1bAeZ28oC-BPk' 
-USER_AGENT = 'just indian things post comment bot v1.0 by /u/justindianthings'
+# THIS BOT IS FOR NON INDIAN SUBS
+# Already run on  - 'memes','meme','subtleasiantraits'
+subredditnames = ['askreddit', 'jokes', 'funny']
+for mysub in subredditnames:    
+    print(' ')
+    print('---')
+    print('Subreddit : '+mysub)        
 
-# Create the Reddit instance and log in
-reddit = praw.Reddit('bot')
+    SUBREDDIT_NAME = mysub
+    KEYWORDS = ['asian parent','asian kid','indian parent','indian kid', 'so india','desi things', 'things india', 'subtle india', 'indian trait', 'bakchodi', 'indians do', 'india']
+    USERNAME = 'justindianstuff'
+    PASSWORD = 'Robinishood69'
+    CLIENT_ID = 'VRi20mq9xuDKuQ'
+    CLIENT_SECRET = 'YRp4P0Ep6p0ewL1bAeZ28oC-BPk' 
+    USER_AGENT = 'just indian things post comment bot v1.0 by /u/justindianthings'
 
-print("Authenticating...")
-reddit = praw.Reddit(
-    client_id=CLIENT_ID,
-    client_secret=CLIENT_SECRET,
-    password=PASSWORD,
-    user_agent=USER_AGENT,
-    username=USERNAME)
-print("Authenticaed as {}".format(reddit.user.me()))
+    # Create the Reddit instance and log in
+    reddit = praw.Reddit('bot')
 
-# Create a list
-if not os.path.isfile("posts_replied_to.txt"):
-    posts_replied_to = []
+    print("Authenticating...")
+    reddit = praw.Reddit(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        password=PASSWORD,
+        user_agent=USER_AGENT,
+        username=USERNAME)
+    print("Authenticaed as {}".format(reddit.user.me()))
 
-# Or load the list of posts we have replied to
-else:
-    with open("posts_replied_to.txt", "r") as f:
-        posts_replied_to = f.read()
-        posts_replied_to = posts_replied_to.split("\n")
-        posts_replied_to = list(filter(None, posts_replied_to))
+    # Create a list
+    if not os.path.isfile("posts_replied_to.txt"):
+        posts_replied_to = []
 
-# Pull the hottest 10 entries from a subreddit of your choosing
-subreddit = reddit.subreddit(SUBREDDIT_NAME)
-for submission in subreddit.new(limit=1000):
-    print(submission.title)
+    # Or load the list of posts we have replied to
+    else:
+        with open("posts_replied_to.txt", "r") as f:
+            posts_replied_to = f.read()
+            posts_replied_to = posts_replied_to.split("\n")
+            posts_replied_to = list(filter(None, posts_replied_to))
 
-    # Make sure you didn't already reply to this post
-    if submission.id not in posts_replied_to:
+    # Pull the hottest 10 entries from a subreddit of your choosing
+    commsadded = 0
+    subreddit = reddit.subreddit(SUBREDDIT_NAME)
 
-        # Not case sensitive
-        # if re.search("Indian mom", submission.title, re.IGNORECASE):
-        # if has_keyword = any(k.lower() in post.title.lower() for k in KEYWORDS):
-        if any(k.lower() in submission.title.lower() for k in KEYWORDS):
-            # Reply
-            submission.reply("This seems like a r/subtleindiantraits moment. Sent by a bot. Beep boop borp, I have kissed zorg.")
-            print("Bot replying to : ", submission.title)
+    print('new')
 
-            # Store id in list
-            posts_replied_to.append(submission.id)
+    for submission in subreddit.new(limit=1000):
+        # print(submission.title)
 
-# Write updated list to file
-with open("posts_replied_to.txt", "w") as f:
-    for post_id in posts_replied_to:
-        f.write(post_id + "\n")
+        # Make sure you didn't already reply to this post
+        if (submission.id not in posts_replied_to) and (submission.over_18 == False) and (submission.archived == False):
+
+            # Not case sensitive
+            # if re.search("Indian mom", submission.title, re.IGNORECASE):
+            # if has_keyword = any(k.lower() in post.title.lower() for k in KEYWORDS):
+            if any(k.lower() in submission.title.lower() for k in KEYWORDS):
+                # Reply
+                submission.reply("This seems like a r/subtleindiantraits moment. Sent by a bot. Beep boop borp, I have kissed zorg.")
+                print("Bot replying to : ", submission.title)
+
+                # Store id in list
+                posts_replied_to.append(submission.id)
+                commsadded = commsadded + 1
+
+
+    # Write updated list to file
+    with open("posts_replied_to.txt", "w") as f:
+        for post_id in posts_replied_to:
+            f.write(post_id + "\n")
+
+    print('rising')
+
+    for submission in subreddit.rising(limit=1000):
+        # print(submission.title)
+
+        # Make sure you didn't already reply to this post
+        if (submission.id not in posts_replied_to) and (submission.over_18 == False) and (submission.archived == False):
+
+            # Not case sensitive
+            # if re.search("Indian mom", submission.title, re.IGNORECASE):
+            # if has_keyword = any(k.lower() in post.title.lower() for k in KEYWORDS):
+            if any(k.lower() in submission.title.lower() for k in KEYWORDS):
+                # Reply
+                submission.reply("This seems like a r/subtleindiantraits moment. Sent by a bot. Beep boop borp, I have kissed zorg.")
+                print("Bot replying to : ", submission.title)
+
+                # Store id in list
+                posts_replied_to.append(submission.id)
+                commsadded = commsadded + 1
+
+    # Write updated list to file
+    with open("posts_replied_to.txt", "w") as f:
+        for post_id in posts_replied_to:
+            f.write(post_id + "\n")
+
+    print('hot')
+
+    for submission in subreddit.hot(limit=1000):
+        # print(submission.title)
+
+        # Make sure you didn't already reply to this post
+        if (submission.id not in posts_replied_to) and (submission.over_18 == False) and (submission.archived == False):
+
+            # Not case sensitive
+            # if re.search("Indian mom", submission.title, re.IGNORECASE):
+            # if has_keyword = any(k.lower() in post.title.lower() for k in KEYWORDS):
+            if any(k.lower() in submission.title.lower() for k in KEYWORDS):
+                # Reply
+                submission.reply("This seems like a r/subtleindiantraits moment. Sent by a bot. Beep boop borp, I have kissed zorg.")
+                print("Bot replying to : ", submission.title)
+
+                # Store id in list
+                posts_replied_to.append(submission.id)
+                commsadded = commsadded + 1
+
+    # Write updated list to file
+    with open("posts_replied_to.txt", "w") as f:
+        for post_id in posts_replied_to:
+            f.write(post_id + "\n")
+
+
+    print('top')
+
+    for submission in subreddit.top(limit=1000):
+        # print(submission.title)
+
+        # Make sure you didn't already reply to this post
+        if (submission.id not in posts_replied_to) and (submission.over_18 == False) and (submission.archived == False):
+
+            # Not case sensitive
+            # if re.search("Indian mom", submission.title, re.IGNORECASE):
+            # if has_keyword = any(k.lower() in post.title.lower() for k in KEYWORDS):
+            if any(k.lower() in submission.title.lower() for k in KEYWORDS):
+                # Reply
+                submission.reply("This seems like a r/subtleindiantraits moment. Sent by a bot. Beep boop borp, I have kissed zorg.")
+                print("Bot replying to : ", submission.title)
+
+                # Store id in list
+                posts_replied_to.append(submission.id)
+                commsadded = commsadded + 1
+
+
+
+    # Write updated list to file
+    with open("posts_replied_to.txt", "w") as f:
+        for post_id in posts_replied_to:
+            f.write(post_id + "\n")
+
+    print(' ')
+    print('---')
+    print('I added '+str(commsadded)+' comment(s).')        
--- stalkbot.py
+++ stalkbot.py
@@ -3,13 +3,15 @@ import pdb
 import re
 import os
 
-SUBREDDIT_NAME = 'dota2dadjokes'
-KEYWORDS = ['asian parent','asian kid','indian parent','indian kid', 'so india','desi things', 'things india', 'subtle india', 'indian trait', 'bakchodi', 'indians do', 'india']
+SUBREDDIT_NAME = 'all'
+KEYWORDS = ['just indian', 'indian things', 'subtle indian', 'indian traits', 'bakchodi']
 USERNAME = 'justindianstuff'
 PASSWORD = 'Robinishood69'
 CLIENT_ID = 'VRi20mq9xuDKuQ'
 CLIENT_SECRET = 'YRp4P0Ep6p0ewL1bAeZ28oC-BPk' 
 USER_AGENT = 'just indian things post comment bot v1.0 by /u/justindianthings'
+
+redditor = reddit.redditor('apocalyptic_cow')
 
 # Create the Reddit instance and log in
 reddit = praw.Reddit('bot')
@@ -36,20 +38,20 @@ else:
 
 # Pull the hottest 10 entries from a subreddit of your choosing
 subreddit = reddit.subreddit(SUBREDDIT_NAME)
-for submission in subreddit.new(limit=10):
+for submission in redditor.posts.new(limit=5):
     print(submission.title)
 
     # Make sure you didn't already reply to this post
-    if (submission.id not in posts_replied_to) and (submission.over_18 == False) and (submission.archived == False):
+    # if submission.id not in posts_replied_to:
 
-        # Not case sensitive
-        if any(k.lower() in submission.title.lower() for k in KEYWORDS):
-            # Reply
-            submission.reply("This seems like a r/subtleindiantraits moment. Sent by a bot. Beep boop borp, I have kissed zorg.")
-            print("Bot replying to : ", submission.title)
+    #     # Not case sensitive
+    #     # if re.search("Indian mom", submission.title, re.IGNORECASE):
+    #     # if has_keyword = any(k.lower() in post.title.lower() for k in KEYWORDS):
+    #     submission.reply("This seems like a r/subtleindiantraits moment. Sent by a bot. Beep boop borp, I have kissed zorg.")
+    #     print("Bot replying to : ", submission.title)
 
-            # Store id in list
-            posts_replied_to.append(submission.id)
+    #     # Store id in list
+    #     posts_replied_to.append(submission.id)
 
 # Write updated list to file
 with open("posts_replied_to.txt", "w") as f:

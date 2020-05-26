f6c2e97eca262c1b67fcebd81aed440f785ba0c2
thephoenix01 <soumyadeep.das.phy14@iitbhu.ac.in>
2020-05-26 16:45:03 +0530

    Removed ouijabot from sh queue. New runs.

--- runbots.sh
+++ runbots.sh
@@ -1,12 +1,9 @@
-echo "Running these bots - comment_bot, message_bot, askouija_bot, delete_comment_bot."
+echo "Running these bots - comment_bot, message_bot, delete_comment_bot."
 echo "Running comment_bot..."
 python3 comment_bot.py 
 echo "---"
 echo "Running message_bot..."
 python3 message_bot.py 
-echo "---"
-echo "Running askouija_bot..."
-python3 askouija_bot.py 
 echo "---"
 echo "Running delete_comment_bot..."
 python3 delete_comment_bot.py

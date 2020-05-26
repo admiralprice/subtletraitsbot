07cc180c5b4c85e3f0a6a8697723f05fba513abb
thephoenix01 <soumyadeep.das.phy14@iitbhu.ac.in>
2020-05-26 12:22:11 +0530

    New runs.

--- runbots.sh
+++ runbots.sh
@@ -1,13 +1,13 @@
 echo "Running these bots - comment_bot, message_bot, askouija_bot, delete_comment_bot."
 echo "Running comment_bot..."
 python3 comment_bot.py 
-print "---"
+echo "---"
 echo "Running message_bot..."
 python3 message_bot.py 
-print "---"
+echo "---"
 echo "Running askouija_bot..."
 python3 askouija_bot.py 
-print "---"
+echo "---"
 echo "Running delete_comment_bot..."
 python3 delete_comment_bot.py
 echo "Fin."

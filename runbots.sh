c8e7ac3534238cb0093023dc9dbc9a94517e7cb5
thephoenix01 <soumyadeep.das.phy14@iitbhu.ac.in>
2020-06-16 11:19:46 +0530

    New run : 0+740 msgs

--- runbots.sh
+++ runbots.sh
@@ -1,6 +1,6 @@
-echo "Running these bots - comment_bot, message_bot, delete_comment_bot."
-echo "Running comment_bot..."
-python3 comment_bot.py 
+echo "Running these bots - non-indian-sub-msg.py, message_bot, delete_comment_bot."
+echo "Running non-indian-sub-msg..."
+python3 non-indian-sub-msg.py 
 echo "---"
 echo "Running message_bot..."
 python3 message_bot.py 
